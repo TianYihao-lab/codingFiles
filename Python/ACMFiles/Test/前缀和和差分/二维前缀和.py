644a492e704227e458661f596ac2b3bbd3239683
TianYihao-lab <201595977+TianYihao-lab@users.noreply.github.com>
2026-05-29 23:34:47 +0800

    Python文件提交

--- Python/ACMFiles/Test/前缀和和差分/二维前缀和.py
+++ Python/ACMFiles/Test/前缀和和差分/二维前缀和.py
@@ -10,6 +10,7 @@ for i in range(1,n+1):
 def find(x1,y1,x2,y2):
     return _Sum[x1][y1]-_Sum[x2-1][y1]-_Sum[x1][y2-1]+_Sum[x2-1][y2-1]
 
+
 for _ in range(q):
     x1,y1,x2,y2=map(int,input().split())
     print(find(x2,y2,x1,y1))
